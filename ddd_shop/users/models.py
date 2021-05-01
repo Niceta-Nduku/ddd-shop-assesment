fbeec90ab55bafaef357922a8026f0927ffc7c0c
Niceta-Nduku <nicetanduku@gmail.com>
2021-05-01 16:50:49 +0300

    Added models for ddd_store

--- ddd_shop/users/models.py
+++ ddd_shop/users/models.py
@@ -4,15 +4,18 @@ from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
 
+class StoreItem(models.Model):
+
+    name = models.CharField(max_length=50)
+    decription = models.CharField(max_length=100)  # extra information
+    sale_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+
 class User(AbstractUser):
     """Default user for ddd_shop."""
 
-    ADMIN = 1
-    STOREOWNER = 2 
-    STOREATTENDANT = 3
-
     #: First and last name do not cover name patterns around the globe
-    name = CharField(_("Name of User"), blank=True, max_length=255)
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
     first_name = None  # type: ignore
     last_name = None  # type: ignore
 
@@ -25,18 +28,31 @@ class User(AbstractUser):
         """
         return reverse("users:detail", kwargs={"username": self.username})
 
-# class StoreOwner(models.Model):
-# user = models.
-#email
 
-# 
+class StoreOwner(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-#     def __str__(self):
-#         return f
 
-# class StoreAttendant(models.Model):
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    items = models.ManyToManyField(StoreItem, blank=True)
+    # defaultOwner = User.objects.get(username='SuperAdmin')
+    owner = models.ForeignKey(StoreOwner, null=True, on_delete=models.PROTECT)#,default=defaultOwner)  # store owner can change
 
-#     def
 
-#class Store(models.Model):
+class StoreAttendant(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE, null=True)
+
+
+# class SaleFile(models.Model):
+
+#     uploadTime = models.DateTimeField()
+#     defaultStore = Store.objects.create(name='Imara Daima DDD', location='Imara Daima')
+#     # if store is deleted does all data?
+#     store = models.ForeignKey(Store, default=defaultStore, on_delete=models.CASCADE)
+#     defaultAttendant = User.objects.get(username='defaultAttendant')  # a dafault attendant
+#     attendant = models.ForeignKey(StoreAttendant, default=defaultAttendant, on_delete=models.PROTECT)
+# #     # fileName = models.FileField(upload_to=) #TODO: determine upload location
 
