3f29f23a1b18642683b6c6499eb600c4e7a18e46
Niceta-Nduku <nicetanduku@gmail.com>
2021-05-03 02:29:45 +0300

    models for apps modified

--- ddd_shop/sales/models.py
+++ ddd_shop/sales/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from users.models import StoreAttendant
+from ddd_shop.users.models import StoreAttendant
+from ddd_shop.shop.models import Store
 
 class StoreItem(models.Model):
 
@@ -11,16 +12,33 @@ class StoreItem(models.Model):
         return self.name
 
 class Sales(models.Model):
-    item = models.ForeignKey(to=StoreItem,on_delete=DO_NOTHING)
+    item = models.ForeignKey(to=StoreItem,on_delete=models.DO_NOTHING)
     amount = models.FloatField()
     comment = models.CharField(max_length=200,blank=True,null=True)
+    date = models.DateField(auto_created=True)
 
     def __str__(self):
         return f'{self.item},{self.amount}'
 
+def file_directory_path(instance, filename):
+    """
+    File path for uploading sale file. 
+    "MEDIA_ROOT/<store_name>/<date of Upload>/<uploaddatetime_attendant_fileID>
+    """
+    extension = filename.split('.')[-1] #file extension
+    store = instance.store.name #
+    date = instance.date#day of sale
+    attendant = instance.attendant.user.username
+    date_time = instance.uploadTime 
+
+    filename = '{0}_{1}_{2}.{3}'.format(date_time,attendant,instance.id,extension)
+    return '{0}/{1}/{2}'.format(store,date,filename)
+
 class SaleFile(models.Model):
 
-    uploadTime = models.DateTimeField()
-    attendant = models.ForeignKey(StoreAttendant, on_delete=models.DO_NOTHING)
-    # fileName = models.FileField(upload_to=) #TODO: determine upload location
+    uploadTime = models.DateTimeField(auto_now=True)
+    date = models.DateField(auto_created=True) #date of sale
+    store = models.ForeignKey(Store, on_delete=models.DO_NOTHING)
+    attendant = models.ForeignKey(StoreAttendant, on_delete=models.DO_NOTHING,null=True)
+    fileName = models.FileField(upload_to=file_directory_path)
 
--- ddd_shop/users/models.py
+++ ddd_shop/users/models.py
@@ -1,58 +1,46 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group
 from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
-
-
-class StoreItem(models.Model):
-
-    name = models.CharField(max_length=50)
-    decription = models.CharField(max_length=100)  # extra information
-    sale_price = models.DecimalField(max_digits=10, decimal_places=2)
+from ddd_shop.shop.models import Store
 
 
 class User(AbstractUser):
     """Default user for ddd_shop."""
-
+    
     #: First and last name do not cover name patterns around the globe
     name = models.CharField(_("Name of User"), blank=True, max_length=255)
-    first_name = None  # type: ignore
-    last_name = None  # type: ignore
+    first_name = None
+    last_name = None
+
+    is_owner = models.BooleanField(
+        _('owner status'),
+        default=False,
+        help_text="Designates whether this user is the owner of a store",
+    )
+    is_attendant = models.BooleanField(
+        _('attendant status'),
+        default=False,
+        help_text="Designates whether this user is a store attendant",
+    )
 
     def get_absolute_url(self):
         """Get url for user's detail view.
-
         Returns:
             str: URL for user detail.
-
         """
         return reverse("users:detail", kwargs={"username": self.username})
+
+    def __str__(self):
+        return self.name
 
 
 class StoreOwner(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-
-class Store(models.Model):
-    name = models.CharField(max_length=100)
-    location = models.CharField(max_length=100)
-    items = models.ManyToManyField(StoreItem, blank=True)
-    # defaultOwner = User.objects.get(username='SuperAdmin')
-    owner = models.ForeignKey(StoreOwner, null=True, on_delete=models.PROTECT)#,default=defaultOwner)  # store owner can change
+    store = models.ManyToManyField(to=Store, blank=True)  # made this to prevent circular import
 
 
 class StoreAttendant(models.Model):
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    store = models.ForeignKey(Store, on_delete=models.CASCADE, null=True)
-
-
-# class SaleFile(models.Model):
-
-#     uploadTime = models.DateTimeField()
-#     defaultStore = Store.objects.create(name='Imara Daima DDD', location='Imara Daima')
-#     # if store is deleted does all data?
-#     store = models.ForeignKey(Store, default=defaultStore, on_delete=models.CASCADE)
-#     defaultAttendant = User.objects.get(username='defaultAttendant')  # a dafault attendant
-#     attendant = models.ForeignKey(StoreAttendant, default=defaultAttendant, on_delete=models.PROTECT)
-# #     # fileName = models.FileField(upload_to=) #TODO: determine upload location
-
+    shop = models.ForeignKey(to=Store, on_delete=models.DO_NOTHING)
